8b51f3e5131415751c9bc8641a29959f4c4ebe26
Vamshi-Krishna1703 <vamshikrishna.amanaganti@gmail.com>
2021-03-31 23:17:01 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { AppliedjobsComponent } from './appliedjobs/appliedjobs.component';
+import { HomecomponentComponent } from './homecomponent/homecomponent.component';
+import { HrappliedjobsComponent } from './hrappliedjobs/hrappliedjobs.component';
+import { HrhomeComponent } from './hrhome/hrhome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'appliedJobs', component:AppliedjobsComponent},
+  {path:'home',component:HomecomponentComponent},
+  {path:'hr',component : HrhomeComponent},
+  {path:'allAppliedJobs',component:HrappliedjobsComponent},
+  {path:'admin',component:AdminhomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppliedjobsComponent } from './appliedjobs/appliedjobs.component';
+import { CusnavbarComponent } from './cusnavbar/cusnavbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomecomponentComponent } from './homecomponent/homecomponent.component';
+import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { HrhomeComponent } from './hrhome/hrhome.component';
+import { HrappliedjobsComponent } from './hrappliedjobs/hrappliedjobs.component';
+import { HrresultComponent } from './hrresult/hrresult.component';
+import { HrnavbarComponent } from './hrnavbar/hrnavbar.component';
+import { AdminnavbarComponent } from './adminnavbar/adminnavbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppliedjobsComponent,
+    CusnavbarComponent,
+    HomecomponentComponent,
+    AdminhomeComponent,
+    HrhomeComponent,
+    HrappliedjobsComponent,
+    HrresultComponent,
+    HrnavbarComponent,
+    AdminnavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
